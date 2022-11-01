b5f43929baefbd0153b0ca98419dd3cbc91be4de
tongxianlong <xianlong.tong@cwdata.com>
2022-11-01 18:08:56 +0800

    学习项目框架

---
+++ cloud-provider-payment8001/src/main/java/com/example/entities/payment.java
@@ -0,0 +1,20 @@
+package com.example.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @ClassName payment
+ * @Description TODO
+ * @Author tongx
+ * @Date 2022/11/1 17:45
+ * @Version 1.0
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class payment {
+    private Long id;
+    private String serial;
+}
